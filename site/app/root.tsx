eb8f2315289b0fa01ae594622315793ad7545bc2
daguitosama <tocarralero@gmail.com>
2024-06-16 14:54:52 -0400

    feat: fav icon

--- site/app/root.tsx
+++ site/app/root.tsx
@@ -1,11 +1,34 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-import global_styles from "~/global.css?url";
 import font_styles from "~/fonts.css?url";
+import global_styles from "~/global.css?url";
 
 export const links: LinksFunction = () => [
     { rel: "stylesheet", href: global_styles },
     { rel: "stylesheet", href: font_styles },
+    {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+    },
+    {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+    },
+    {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+    },
+    {
+        rel: "manifest",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/site.webmanifest",
+    },
 ];
 
 export default function App() {
